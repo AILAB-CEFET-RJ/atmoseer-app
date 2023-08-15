afefd4b1e349fdcfe9d2f06941dae343fa65c061
Eduardo Moura <79063539+EduMoura-debug@users.noreply.github.com>
2023-08-15 20:37:09 -0300

    .

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# conda environment
+conda env create -f config/environment.yml
+
+mkdir -p ./data
+mkdir -p ./data/ws
+mkdir -p ./data/as
+mkdir -p ./data/NWP
+mkdir -p ./data/NWP/ERA5
+mkdir -p ./data/datasets
+
+# docker image
+#TODO docker build -t atmoseer .
