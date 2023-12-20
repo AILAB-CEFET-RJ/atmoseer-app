941890692539aedd8850be54b96ee24a0d58ee71
Mateus Sotero <49726164+mateussotero@users.noreply.github.com>
2023-12-20 15:52:40 -0300

    Mudanças na pasta src do Atmoseer

--- src/globals.py
+++ src/globals.py
@@ -1,13 +1,21 @@
 INMET_API_BASE_URL = "https://apitempo.inmet.gov.br"
 
-# Weather station datasource directory
+# Weather stations datasource directories
 WS_INMET_DATA_DIR = "./data/ws/inmet/"
+WS_ALERTARIO_DATA_DIR = "./data/ws/alertario/ws/"
+GS_ALERTARIO_DATA_DIR = "./data/ws/alertario/rain_gauge_era5_fused/"
+
+# WS_GOES_DATA_DIR = "atmoseer/data/ws/goes16"
+GOES_DATA_DIR = "./data/goes16"
 
 # Atmospheric sounding datasource directory
 NWP_DATA_DIR = "./data/NWP/"
 
 # Atmospheric sounding datasource directory
 AS_DATA_DIR = "./data/as/"
+
+# TPW GOES16 datasource directory
+TPW_DATA_DIR = "./data/goes16/wsoi"
 
 # Directory to store the train/val/test datasets for each weather station of interest
 DATASETS_DIR = './data/datasets/'
@@ -16,40 +24,72 @@ DATASETS_DIR = './data/datasets/'
 MODELS_DIR = './models/'
 
 # see https://portal.inmet.gov.br/paginas/catalogoaut
-INMET_STATION_CODES_RJ = ('A636', 
-                          'A621', 
-                          'A602', 
-                          'A652',
-                          'A627')
+INMET_WEATHER_STATION_IDS = (
+    'A636', # Jacarepagua
+    'A621', # Vila militar
+    'A602', # Marambaia
+    'A652', # Forte de Copacabana
+    'A627'  # Niteroi
+)
 
-COR_STATION_NAMES_RJ = ('alto_da_boa_vista',
-                        'guaratiba',
-                        'iraja',
-                        'jardim_botanico',
-                        'riocentro',
-                        'santa_cruz',
-                        'sao_cristovao',
-                        'vidigal')
+ALERTARIO_GAUGE_STATION_IDS = (
+                         'anchieta', 
+                         'av_brasil_mendanha', 
+                         'bangu', 
+                         'barrinha', 
+                         'campo_grande', 
+                         'cidade_de_deus', 
+                         'copacabana', 
+                         'grajau_jacarepagua', 
+                         'grajau', 
+                         'grande_meier', 
+                         'grota_funda', 
+                         'ilha_do_governador', 
+                         'laranjeiras', 
+                         'madureira', 
+                         'penha', 
+                         'piedade', 
+                         'recreio', 
+                         'rocinha',
+                         'santa_teresa',
+                         'saude', 
+                         'sepetiba', 
+                         'tanque', 
+                         'tijuca_muda', 
+                         'tijuca', 
+                         'urca',
+                         'alto_da_boa_vista', #**
+                         'iraja', #**
+                         'jardim_botanico', #**
+                         'riocentro', #**
+                         'santa_cruz', #**
+                         'vidigal' #**
+                         )
 
-hyper_params_dict_bc = {
-    "N_EPOCHS" : 3500,
-    "PATIENCE" : 1000,
-    "BATCH_SIZE" : 1024,
-    "WEIGHT_DECAY" : 0,
-    "LEARNING_RATE" : 0.0003,
-    "DROPOUT_RATE" : 0.5,
-    "SLIDING_WINDOW_SIZE" : 6
-}
+ALERTARIO_WEATHER_STATION_IDS = (
+                         'guaratiba', #**
+                         'sao_cristovao' #**
+                         )
 
-hyper_params_dict_oc = {
-    "N_EPOCHS" : 3500,
-    "PATIENCE" : 1000,
-    "BATCH_SIZE" : 1024,
-    "WEIGHT_DECAY" : 0,
-    "LEARNING_RATE" : 3e-5,
-    "DROPOUT_RATE" : 0.5,
-    "SLIDING_WINDOW_SIZE" : 6
-}
+# hyper_params_dict_bc = {
+#     "N_EPOCHS" : 3500,
+#     "PATIENCE" : 1000,
+#     "BATCH_SIZE" : 1024,
+#     "WEIGHT_DECAY" : 0,
+#     "LEARNING_RATE" : 0.0003,
+#     "DROPOUT_RATE" : 0.5,
+#     "SLIDING_WINDOW_SIZE" : 6
+# }
+
+# hyper_params_dict_oc = {
+#     "N_EPOCHS" : 6000,
+#     "PATIENCE" : 1000,
+#     "BATCH_SIZE" : 1024,
+#     "WEIGHT_DECAY" : 0,
+#     "LEARNING_RATE" : 3e-6,
+#     "DROPOUT_RATE" : 0.5,
+#     "SLIDING_WINDOW_SIZE" : 6
+# }
 
 
 # Observed variables for INMET weather stations:
--- src/preprocess_ws.py
+++ src/preprocess_ws.py
@@ -2,10 +2,10 @@ import pandas as pd
 from pathlib import Path
 import argparse
 import sys
-from globals import *
-from util import *
-import util as util
+import globals
+import util
 from sklearn.impute import KNNImputer
+import logging
 
 # list_valid_datasource_combinations = ("R", "N", "R+N")
 
@@ -52,54 +52,101 @@ from sklearn.impute import KNNImputer
 #     print(f"Saving preprocessed data to {filename}")
 #     df.to_parquet(filename, compression='gzip')
 
-def preprocess_ws(station_id, ws_datasource):
-    print(f"Loading datasource file ({ws_datasource}).")
-    df = pd.read_csv(ws_datasource)
+def preprocess_ws(ws_id, ws_filename, output_folder):
+
+    logging.info(f"Loading datasource file {ws_filename}).")
+    df = pd.read_parquet(ws_filename)
+    logging.info(df.head())
+    logging.info("Done!\n")
 
     #
     # Add index to dataframe using the timestamps.
-    df = add_datetime_index(station_id, df)
+    logging.info(f"Adding index to dataframe using the timestamps...")
+    df = util.add_datetime_index(ws_id, df)
+    logging.info(df.head())
+    logging.info("Done!\n")
+
+    #
+    # Standardize column names.
+    logging.info(f"Standardizing column names...")
+    if ws_id in globals.ALERTARIO_WEATHER_STATION_IDS:
+        column_name_mapping = {
+            "datetime": "datetime",
+            "temperature_mean": "temperature",
+            "humidity_mean": "relative_humidity",
+            "pressure_mean": "barometric_pressure",
+            "wind_speed_mean": "wind_speed",
+            "wind_dir_mean": "wind_dir",
+            "precipitation_sum": "precipitation"
+        }
+    elif ws_id in globals.INMET_WEATHER_STATION_IDS:
+        column_name_mapping = {
+            "datetime": "datetime",
+            "TEM_MAX": "temperature",
+            "UMD_MAX": "relative_humidity",
+            "PRE_MAX": "barometric_pressure",
+            "VEN_VEL": "wind_speed",
+            "VEN_DIR": "wind_dir",
+            "CHUVA": "precipitation"
+        }
+    column_names = column_name_mapping.keys()
+    df = util.get_dataframe_with_selected_columns(df, column_names)
+    df = util.rename_dataframe_column_names(df, column_name_mapping)
+    logging.info(df.head())
+    logging.info("Done!\n")
+
+    logging.info("Getting relevant variables...")
+    predictor_names, target_name = util.get_relevant_variables(ws_id)
+    logging.info(f"Predictors: {predictor_names}")
+    logging.info(f"Target: {target_name}")
+    logging.info("Done!\n")
 
     #
     # Drop observations in which the target variable is not defined.
-    print(f"Dropping entries with null target.")
+    logging.info(f"Dropping entries with null target...")
     n_obser_before_drop = len(df)
-    df = df[df['CHUVA'].notna()]
+    df = df[df[target_name].notna()]
     n_obser_after_drop = len(df)
-    print(f"Number of observations before/after dropping entries with undefined target value: {n_obser_before_drop}/{n_obser_after_drop}.")
-    print(f"Range of timestamps after dropping entries with undefined target value: [{min(df.index)}, {max(df.index)}]")
+    logging.info(f"Number of observations before/after dropping entries with undefined target value: {n_obser_before_drop}/{n_obser_after_drop}.")
+    logging.info(f"Range of timestamps after dropping entries with undefined target value: [{min(df.index)}, {max(df.index)}]")
+    logging.info(df.head())
+    logging.info("Done!\n")
 
     #
     # Create wind-related features (U and V components of wind observations).
-    df = add_wind_related_features(station_id, df)
+    logging.info(f"Creating wind-related features...")
+    df = util.add_wind_related_features(ws_id, df)
+    logging.info(df.head())
+    logging.info("Done!\n")
 
     #
-    # Create hour-related features (sin and cos components)
-    df = add_hour_related_features(df)
+    # Create time-related features (sin and cos components)
+    logging.info(f"Creating time-related features...")
+    df = util.add_hour_related_features(df)
+    logging.info(df.head())
+    logging.info("Done!\n")
 
-    predictor_names, target_name = get_relevant_variables(station_id)
-    print(f"Chosen predictors: {predictor_names}")
-    print(f"Chosen target: {target_name}")
     df = df[predictor_names + [target_name]]
 
     #
     # Normalize the weather station data. This step is necessary here due to the next step, which deals with missing values.
     # Notice that we drop the target column before normalizing, to avoid some kind of data leakage.
     # (see https://stats.stackexchange.com/questions/214728/should-data-be-normalized-before-or-after-imputation-of-missing-data)
-    print("Normalizing data before applying KNNImputer...", end='')
+    logging.info("Min-max normalizing data...")
     target_column = df[target_name]
     df = df.drop(columns=[target_name], axis=1)
-    df = min_max_normalize(df)
-    print("Done!")
+    df = util.min_max_normalize(df)
+    logging.info("Done!\n")
 
     # 
     # Imput missing values on some features.
+    logging.info("Applying KNNImputer...")
     percentage_missing = (df.isna().mean() * 100).mean() # Compute the percentage of missing values
-    print(f"There are {df.isnull().sum().sum()} missing values ({percentage_missing:.2f}%). Going to fill them...", end = '')
+    logging.info(f"There are {df.isnull().sum().sum()} missing values ({percentage_missing:.2f}%). Going to fill them...")
     imputer = KNNImputer(n_neighbors=2)
     df[:] = imputer.fit_transform(df)
     assert (not df.isnull().values.any().any())
-    print("Done!")
+    logging.info("Done!\n")
 
     #
     # Add the target column back to the DataFrame.
@@ -107,39 +154,40 @@ def preprocess_ws(station_id, ws_datasource):
 
     #
     # Save preprocessed data to a parquet file.
-    filename_and_extension = get_filename_and_extension(ws_datasource)
-    filename = WS_INMET_DATA_DIR + filename_and_extension[0] + '_preprocessed.parquet.gzip'
-    print(f"Saving preprocessed data to {filename}")
+    filename_and_extension = util.get_filename_and_extension(ws_filename)
+    filename = output_folder + filename_and_extension[0] + '_preprocessed.parquet.gzip'
+    logging.info(f"Saving preprocessed data to {filename}...")
     df.to_parquet(filename, compression='gzip')
+    logging.info("Done!\n")
+
+    logging.info("Done it all!\n")
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Preprocess weather station data.')
-    parser.add_argument('-s', '--station_id', required=True, choices=INMET_STATION_CODES_RJ + COR_STATION_NAMES_RJ, help='ID of the weather station to preprocess data for.')
-    # parser.add_argument('-d', '--datasources', required=True, choices=list_valid_datasource_combinations, help='Data sources to preprocess. Combination of R (sounding indices) and N (NWP data) allowed.')
-    # parser.add_argument('-n', '--neighbors', default=0, type=int, help='Number of neighbor weather stations to use.')
+    parser.add_argument('-s', '--station_id', 
+                        required=True, 
+                        choices=globals.INMET_WEATHER_STATION_IDS + globals.ALERTARIO_WEATHER_STATION_IDS, 
+                        help='ID of the weather station to preprocess data for.')
     args = parser.parse_args(argv[1:])
     
-    # sounding_indices_data_source = None
-    # numerical_model_data_source = None
-    
-    # if args.datasources.find('R') != -1:
-    #     sounding_indices_data_source = '../data/sounding/SBGL_indices_1997_2023.parquet.gzip'
-    # if args.datasources.find('N') != -1:
-    #     numerical_model_data_source = '../data/NWP/ERA5_A652_1997_2023.csv'
+    station_id = args.station_id
 
-    # print(f'Going to preprocess data sources according to user specification ({args.datasources})...')
+    fmt = "[%(levelname)s] %(funcName)s():%(lineno)i: %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format = fmt)
 
-    print('\n***Preprocessing weather station data***')
-    ws_datasource = WS_INMET_DATA_DIR + args.station_id + ".csv"
-    preprocess_ws(args.station_id, ws_datasource)
-    
-    # if sounding_indices_data_source is not None:
-    #     print('\n***Preprocessing sounding indices data***')
-    #     preprocess_sounding_data(sounding_indices_data_source)
+    if not ((station_id in globals.INMET_WEATHER_STATION_IDS) or (station_id in globals.ALERTARIO_WEATHER_STATION_IDS)):
+        print(f"Invalid station identifier: {station_id}")
+        parser.print_help()
+        sys.exit(2)
 
-    # if numerical_model_data_source is not None:
-    #     print('\n***Preprocessing NWP data***')
-    #     preprocess_numerical_model_data(numerical_model_data_source)
+    if (station_id in globals.INMET_WEATHER_STATION_IDS):
+        ws_data_dir = globals.WS_INMET_DATA_DIR
+    elif (station_id in globals.ALERTARIO_WEATHER_STATION_IDS):
+        ws_data_dir = globals.WS_ALERTARIO_DATA_DIR
+
+    print(f'Preprocessing data coming from weather station {station_id}')
+    ws_filename = ws_data_dir + args.station_id + ".parquet"
+    preprocess_ws(ws_id=station_id, ws_filename=ws_filename, output_folder=ws_data_dir)
 
     print('Done!')
 
