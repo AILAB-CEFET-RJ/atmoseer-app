4e839c135a4ae901a70350e6367bbf9febeddf26
Mateus Sotero <49726164+mateussotero@users.noreply.github.com>
2023-12-20 16:45:15 -0300

    adicionando a rota predict.js

--- backend/app.js
+++ backend/app.js
@@ -1,37 +1,47 @@
-//   Importando Express: um framework para Node.js voltado para o desenvolvimento web, servindo como uma camada de abstração 
-// para simplificar a criação de aplicativos e APIs web
-const express = require('express')
-//   Importando Mongoose: uma biblioteca do Node.js que trabalha com o MongoDB, um banco de dados do NoSQL, criando uma 
-// camada de abstração que facilita na interação com os documentos do banco de dados
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 
-//Definição da URL de conexão com o MongoDB
-const mongoURI = 'mongodb://127.0.0.1:27017/db' //'mongodb://localhost/db'
 
-// Initializando Express.js
-const app = express()
+const app = express();
+const port = 3000; 
 
-// Configuração de porta 3000 e exibição de mensagem no console como confirmação
-app.listen(3000, () => {
-   console.log('Listening for request on port 3000');
+
+const soundingRouter = require('./routes/Sounding');
+const corRouter = require('./routes/COR');
+const inmetRouter = require('./routes/Inmet');
+const predictRouter = require('./routes/Predict');
+// const era5Router = require('./routes/Era5');
+
+app.use(express.json()); // Permite o uso de JSON
+
+
+const mongoURI = 'mongodb://127.0.0.1:27017/db';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('MongoDB connected');
+
+  
+  app.use('/sounding', soundingRouter);
+  app.use('/cor', corRouter);
+  app.use('/inmet', inmetRouter);
+  // app.use('/era5', era5Router);
+  app.use('/predict', predictRouter);
+
+  
+  app.listen(port, () => {
+    console.log(`Listening for requests on port ${port}`);
+  });
 });
 
-// Importando os dados das quatro fontes: Sondas, COR, InMet e Era5
-const soundingRouter = require('./routes/Sounding')
-const corRouter = require('./routes/COR')
-const inmetRouter = require('./routes/Inmet')
-//const era5Router = require('./routes/Era5')
 
-// Definição das rotas de cada fonte
-app.use('/sounding', soundingRouter)
-app.use('/cor', corRouter)
-app.use('/inmet', inmetRouter)
-//app.use('/era5', era5Router)
+app.use((err, req, res, next) => {
+   console.error(err.stack);
+   res.status(500).send('Algo deu errado!');
+ });
+ 
 
-// Inicialização e armazenamento da conexão no URL definido
-mongoose.connect(mongoURI)
-db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-
-// Exportando para o objeto app
 module.exports = app
