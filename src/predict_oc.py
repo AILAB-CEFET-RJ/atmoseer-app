e5855cdf81679352f740c58ad15865407ab4725a
Mateus Sotero <49726164+mateussotero@users.noreply.github.com>
2023-12-20 16:12:52 -0300

    add a func receber estação e tratamento de erros

--- src/predict_oc.py
+++ src/predict_oc.py
@@ -1,48 +1,58 @@
 import torch
 import torch.nn as nn
-from src.train.ordinal_classifier import OrdinalClassifier
+from train.ordinal_classifier import OrdinalClassifier
 import globals as globals
 import pickle
 import yaml
+import sys 
+
+
 
 if __name__ == "__main__":
-    pipeline_id = "A652_N"
+    try:
+        estacao = sys.argv[1]
+        pipeline_id = estacao + "_N"
 
-    #
-    # Load numpy arrays (stored in a pickle file) from disk
-    filename = globals.DATASETS_DIR + pipeline_id + ".pickle"
-    file = open(filename, 'rb')
-    (_, _, _, _, X_test, _) = pickle.load(file)
-    print(f"Shape of test data matrix: {X_test.shape}")
+        #
+        # Load numpy arrays (stored in a pickle file) from disk
+        filename = globals.DATASETS_DIR + estacao + ".pickle"
+        file = open(filename, 'rb')
+        (_, _, _, _, X_test, _) = pickle.load(file)
+        print(f"Shape of test data matrix: {X_test.shape}")
 
-    # Example to predict is the firs one in the test dataset.
-    x = X_test[0:1, :, :]
-    print(f"Shape of one test example: {x.shape}")
-    print("Model input:")
-    print(x)
+        # Example to predict is the firs one in the test dataset.
+        x = X_test[0:1, :, :]
+        print(f"Shape of one test example: {x.shape}")
+        print("Model input:")
+        print(x)
 
-    NUM_FEATURES = X_test.shape[2]
-    NUM_CLASSES = 5
+        NUM_FEATURES = X_test.shape[2]
+        NUM_CLASSES = 5
 
-    with open('./config/config.yaml', 'r') as file:
-        config = yaml.safe_load(file)
+        with open('./config/config.yaml', 'r') as file:
+            config = yaml.safe_load(file)
 
-    #Create an instance of the model
-    input_dim = (NUM_FEATURES, config["preproc"]["SLIDING_WINDOW_SIZE"])
-    model = OrdinalClassifier(in_channels=NUM_FEATURES, 
-                                    num_classes=NUM_CLASSES, 
-                                    input_dim = input_dim, 
-                                    target_average = None)
+        #Create an instance of the model
+        input_dim = (NUM_FEATURES, config["preproc"]["SLIDING_WINDOW_SIZE"])
+        model = OrdinalClassifier(in_channels=NUM_FEATURES, 
+                                        num_classes=NUM_CLASSES, 
+                                        input_dim = input_dim, 
+                                        target_average = None)
 
-    # Load the pretrained model weights from the file
-    model_path = globals.MODELS_DIR + "best_" + pipeline_id + "_OC.pt"  # Path to the pretrained model file
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+        # Load the pretrained model weights from the file
+        model_path = globals.MODELS_DIR + "best_" + pipeline_id + "_OC.pt"  # Path to the pretrained model file
+        model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 
 
-    # Set the model in evaluation (inference) mode.
-    model.eval()
+        # Set the model in evaluation (inference) mode.
+        model.eval()
 
-    # Make prediction using the loaded model
-    with torch.no_grad():
-        output = model.predict(x)
-        print(f"Predicted level: {output[0][0]}")
+        # Make prediction using the loaded model
+        with torch.no_grad():
+            output = model.predict(x)
+            print(f"Predicted level: {output[0][0]}")
+
+    except Exception as e:
+        print("Erro ao executar o script Python")
+        print(e)
+
