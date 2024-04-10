5b1ea946ea0bd3384c590264bc2e58c5e3635782
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-04-10 15:12:51 -0300

    feat estilização página forecast

--- atmoseer_app_frontend/src/pages/Geolocation.tsx
+++ atmoseer_app_frontend/src/pages/Geolocation.tsx
@@ -1,20 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useForecastData } from '../hooks/useForecastData';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe, faMapMarkerAlt  } from '@fortawesome/free-solid-svg-icons';
+import WeatherIcon from '../components/WeatherIcon';
+import '../styles/geolocation.css';
 
 const GeolocationExample: React.FC = () => {
   const [latitude, setLatitude] = useState<number | null>(null);
   const [longitude, setLongitude] = useState<number | null>(null);
+   //const [forecastResult, setForecastResult] = useState<number>(0);
+  const [placeName, setPlaceName] = useState<string>('');
+  const [forecastMessage, setForecastMessage] = useState<string>('Previsão não disponível');
 
-  // Usando o hook useForecastData para obter os dados da previsão com base na latitude e longitude
   const { data: forecastData, isLoading, error } = useForecastData(latitude, longitude);
+
+  
 
   const getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          // Limitando apenas para o estado do Rio de Janeiro
           if (latitude >= -23.4318 && latitude <= -20.7773 && longitude >= -44.7426 && longitude <= -40.7492) {
             setLatitude(latitude);
             setLongitude(longitude);
@@ -27,24 +34,65 @@ const GeolocationExample: React.FC = () => {
         }
       );
     } else {
-      console.error('Geolocation is not supported by this browser.');
+      console.error('Geolocation não é suportado por esse navegador.');
     }
   };
 
   useEffect(() => {
     getLocation();
   }, []);
+  //const manualForecastResult = 4;
+  useEffect(() => {
+    if (forecastData) {
+      setPlaceName(forecastData.name); 
+      const messageParts = forecastData.message.split(':');
+      if (messageParts.length > 1) {
+        setForecastMessage(`Resultado da predição: ${messageParts[1].trim()}`);
+        //setForecastMessage(`Resultado da predição: ${manualForecastResult}`);
+      } else {
+        setForecastMessage('Previsão não disponível');
+      }
+    }
+  }, [forecastData]);
 
   if (isLoading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div>
-      <p>Latitude: {latitude}</p>
-      <p>Longitude: {longitude}</p>
-      {/* Acessando os dados da previsão, se disponíveis */}
-      <p>Forecast: {forecastData ? forecastData.message : 'No forecast data available'}</p>
-      <Link to="/home">Voltar</Link>
+    <div className="container-wrapper">
+      <div id="container">
+        <div className='all_infos'>
+          <div className="info">
+            <FontAwesomeIcon icon={faGlobe} id="latitude_icon" />
+            <div>
+              <h2>Latitude</h2>
+              <p id="latitude">{latitude}</p>
+            </div>
+          </div>
+          <div className="info">
+            <FontAwesomeIcon icon={faGlobe} id="longitude_icon"/>
+            <div>
+              <h2>Longitude</h2>
+              <p id="longitude">{longitude}</p>
+            </div>
+          </div>
+          <div className="info">
+            <WeatherIcon forecastMessage={forecastMessage} />
+            <div>
+              <h2>Previsão do Tempo</h2>
+              <p>{forecastMessage}</p>
+            </div>
+          </div>
+          <div className="info">
+          <FontAwesomeIcon icon={faMapMarkerAlt} id="local_icon" />
+            <div>
+            <h2>Nome do lugar</h2>
+              <p>{placeName}</p>
+            </div>
+          </div>
+        </div>
+        <Link to="/home" className="back-button">Voltar</Link>      
+      </div>
     </div>
   );
 };
