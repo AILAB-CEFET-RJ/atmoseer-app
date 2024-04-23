7ff795202cf4a0fbd852120f918d4c6c4924eda3
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-04-23 17:53:36 -0300

    fix novos dados, inserção de componentes e api openmeteo

--- atmoseer_app_frontend/src/pages/Geolocation.tsx
+++ atmoseer_app_frontend/src/pages/Geolocation.tsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useForecastData } from '../hooks/useForecastData';
+import { useOpenMeteoData } from '../hooks/useOpenMeteoData';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGlobe, faMapMarkerAlt  } from '@fortawesome/free-solid-svg-icons';
+import { faLocationArrow, faTint, faWind, faEarthEurope, faMapLocationDot, faCloudArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
 import WeatherIcon from '../components/WeatherIcon';
+import TemperatureIcon from '../components/TemperatureIcon';
+import WindDirection from '../components/WindDirection';
+import CloudIcon from '../components/CloudIcon';
+import WeatherConditionTranslation from '../components/WeatherConditionTranslation';
 
 const GeolocationExample: React.FC = () => {
   const [latitude, setLatitude] = useState<number | null>(null);
   const [longitude, setLongitude] = useState<number | null>(null);
-   //const [forecastResult, setForecastResult] = useState<number>(0);
   const [placeName, setPlaceName] = useState<string>('');
-  const [forecastMessage, setForecastMessage] = useState<string>('Previsão não disponível');
-
-  const { data: forecastData, isLoading, error } = useForecastData(latitude, longitude);
-
-  
+  const [forecastMessage, setForecastMessage] = useState<string>('');
+  const { data: forecastData, isLoading: forecastLoading, error: forecastError } = useForecastData(latitude, longitude);
+  const { data: openMeteoData, isLoading: openMeteoLoading, error: openMeteoError } = useOpenMeteoData(latitude, longitude);
 
   const getLocation = () => {
     if (navigator.geolocation) {
@@ -40,59 +43,129 @@ const GeolocationExample: React.FC = () => {
   useEffect(() => {
     getLocation();
   }, []);
-  //const manualForecastResult = 4;
+
   useEffect(() => {
-    if (forecastData) {
-      setPlaceName(forecastData.name); 
-      const messageParts = forecastData.message.split(':');
-      if (messageParts.length > 1) {
-        setForecastMessage(`Resultado da predição: ${messageParts[1].trim()}`);
-        //setForecastMessage(`Resultado da predição: ${manualForecastResult}`);
-      } else {
-        setForecastMessage('Previsão não disponível');
-      }
+    if (forecastData && forecastData.atmoseer_result) {
+      const { prediction_mapped, station } = forecastData.atmoseer_result;
+
+      const translationMap = {
+        "no rain": "Sem chuva",
+        "rain": "Chuva",
+        "heavy rain": "Chuva forte",
+        "very heavy rain": "Chuva muito forte",
+        "extreme rain": "Chuva extrema",
+
+      };
+
+      const translatedPrediction = translationMap[prediction_mapped] || prediction_mapped;
+
+      setForecastMessage(`${translatedPrediction}`);
+      setPlaceName(station.name);
     }
   }, [forecastData]);
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+
+  const resetData = () => {
+    setLatitude(null);
+    setLongitude(null);
+    setPlaceName('');
+    setForecastMessage('');
+  };
+
+
+  console.log(forecastData)
+  console.log(openMeteoData)
 
   return (
     <div className='flex justify-center'>
       <div className="flex justify-flex justify-center items-center min-h-screen w-4/5">
         <div className="flex flex-col items-center bg-[#f0f8ffcc] rounded-2xl px-4 py-2 shadow-md">
-          <div className='grid grid-cols-2 mt-2 gap-1'>
-            <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
-              <FontAwesomeIcon icon={faGlobe} className="text-blue-800 text-2xl ml-2" />
-              <div>
-                <h2 className='ml-2 mt-1 text-base font-semibold'>Latitude</h2>
-                <p className="text-base text-gray-800 m-2">{latitude}</p>
-              </div>
+          {forecastLoading || openMeteoLoading ? (
+            <div className="flex flex-col items-center justify-center w-full h-full">
+              <img src="/atmoseer.png" alt="Logo" className="w-32 h-32 mb-4" />
+              <p className="text-center mt-2 text-black-500 font-semibold">Carregando dados...</p>
             </div>
-            <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
-              <FontAwesomeIcon icon={faGlobe} className="text-green-800 text-2xl ml-2"/>
-              <div>
-                <h2 className='ml-2 mt-1 text-base font-semibold'>Longitude</h2>
-                <p className='text-base text-gray-800 m-2'>{longitude}</p>
+          ) : (
+            forecastError || openMeteoError ? (
+              <p className="text-center mt-8 text-red-500 font-semibold">Erro: {forecastError?.message || openMeteoError?.message}</p>
+            ) : (
+              <div className='grid grid-cols-2 mt-2 gap-1'>
+                <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
+                  <FontAwesomeIcon icon={faEarthAmericas} className="text-yellow-400 text-2xl ml-2" />
+                  <div>
+                    <h2 className='ml-2 mt-1 text-base font-semibold'>Latitude</h2>
+                    <p className="text-base text-gray-800 m-2">{latitude}</p>
+                  </div>
+                </div>
+                <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
+                  <FontAwesomeIcon icon={faEarthEurope} className="text-green-800 text-2xl ml-2" />
+                  <div>
+                    <h2 className='ml-2 mt-1 text-base font-semibold'>Longitude</h2>
+                    <p className='text-base text-gray-800 m-2'>{longitude}</p>
+                  </div>
+                </div>
+                <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
+                  <WeatherIcon forecastMessage={forecastMessage} />
+                  <div>
+                    <h2 className='ml-2 mt-1 text-base font-semibold'>Previsão do Tempo</h2>
+                    <p className="text-base text-gray-800 m-2">{forecastMessage}</p>
+                  </div>
+                </div>
+                <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
+                  <CloudIcon condition={openMeteoData?.condition} colorIntensity={undefined} />
+                  <div>
+                    <h2 className='ml-2 mt-1 text-base font-semibold'>Clima Atual</h2>
+                    <WeatherConditionTranslation conditionData={openMeteoData} />
+                  </div>
+                </div>
+                <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
+                  <FontAwesomeIcon icon={faMapLocationDot} className="text-purple-800 text-2xl ml-2" />
+                  <div>
+                    <h2 className='ml-2 mt-1 text-base font-semibold'>Estação de Previsão</h2>
+                    <p className="text-base text-gray-800 m-2">{placeName}</p>
+                  </div>
+                </div>
+                <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
+                  <TemperatureIcon temperature={openMeteoData?.temperature} className="text-2xl ml-2" />
+                  <div>
+                    <h2 className='ml-2 mt-1 text-base font-semibold'>Temperatura Atual</h2>
+                    <p className="text-base text-gray-800 m-2">{openMeteoData?.temperature && `${openMeteoData.temperature} °C`}</p>
+                  </div>
+                </div>
+                <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
+                  <FontAwesomeIcon icon={faCloudArrowDown} className="text-gray-300 text-2xl ml-2" />
+                  <div>
+                    <h2 className='ml-2 mt-1 text-base font-semibold'>Nebulosidade</h2>
+                    <p className="text-base text-gray-800 m-2">{openMeteoData?.cloud_cover && `${openMeteoData.cloud_cover} %`}</p>
+                  </div>
+                </div>
+                <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
+                  <FontAwesomeIcon icon={faTint} className="text-blue-400 text-2xl ml-2" />
+                  <div>
+                    <h2 className='ml-2 mt-1 text-base font-semibold'>Umidade</h2>
+                    <p className="text-base text-gray-800 m-2">{openMeteoData?.humidity && `${openMeteoData.humidity} %`}</p>
+                  </div>
+                </div>
+                <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
+                  <FontAwesomeIcon icon={faWind} className="text-blue-300 text-2xl ml-2" />
+                  <div>
+                    <h2 className='ml-2 mt-1 text-base font-semibold'>Velocidade do Vento</h2>
+                    <p className="text-base text-gray-800 m-2">{openMeteoData?.wind_speed && `${openMeteoData.wind_speed} km/h`}</p>
+                  </div>
+                </div>
+                <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
+                  <FontAwesomeIcon icon={faLocationArrow} className="text-gray-400 text-2xl ml-2" />
+                  <div>
+                    <h2 className='ml-2 mt-1 text-base font-semibold'>Direção do Vento</h2>
+                    {openMeteoData && openMeteoData.wind_direction && (
+                      <WindDirection windDirection={openMeteoData?.wind_direction && `${openMeteoData.temperature}`} />
+                    )}
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
-              <WeatherIcon forecastMessage={forecastMessage} />
-              <div>
-                <h2 className='ml-2 mt-1 text-base font-semibold'>Previsão do Tempo</h2>
-                <p className="text-base text-gray-800 m-2">{forecastMessage}</p>
-              </div>
-            </div>
-            <div className="flex items-center justify-start p-0.5 rounded-lg shadow-sm bg-white gap-2">
-            <FontAwesomeIcon icon={faMapMarkerAlt} className="text-purple-800 text-2xl ml-2" />
-              <div>
-              <h2 className='ml-2 mt-1 text-base font-semibold'>Estação</h2>
-                <p className="text-base text-gray-800 m-2">Nome estação</p>
-              </div>
-            </div>
-          </div>
-          <Link to="/home" className="inline-block px-4 py-2 mt-2 rounded text-white font-bold no-underline bg-blue-500 hover:bg-blue-600 transition-colors">Voltar</Link>      
-        </div>
+            )
+          )}
+          <Link to="/home" className="inline-block px-4 py-2 mt-2 rounded text-white font-bold no-underline bg-blue-500 hover:bg-blue-600 transition-colors" onClick={resetData}>Voltar</Link>        </div>
       </div>
     </div>
   );
