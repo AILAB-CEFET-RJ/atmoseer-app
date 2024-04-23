a17601da170c6e081f05b6dd4671e9bd260b6fe4
wxsleyb <92063559+wxsleyb@users.noreply.github.com>
2024-04-23 17:47:38 -0300

    fix component icon and color temperature

---
+++ atmoseer_app_frontend/src/components/TemperatureIcon.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTemperatureHigh, faTemperatureHalf, faTemperatureLow } from '@fortawesome/free-solid-svg-icons';
+
+const TemperatureIcon = ({ temperature, className }) => {
+  let temperatureIcon, temperatureColor;
+
+  if (temperature < 20) {
+    temperatureIcon = faTemperatureLow;
+    temperatureColor = "text-blue-800";
+  } else if (temperature >= 20 && temperature <= 30) {
+    temperatureIcon = faTemperatureHalf;
+    temperatureColor = "text-orange-500";
+  } else {
+    temperatureIcon = faTemperatureHigh;
+    temperatureColor = "text-red-600";
+  }
+
+  if (!temperature) {
+    temperatureIcon = faTemperatureHigh; 
+    temperatureColor = "text-gray-400";
+  }
+
+  return (
+    <FontAwesomeIcon icon={temperatureIcon} className={`${temperatureColor} ${className}`} />
+  );
+};
+
+export default TemperatureIcon;
